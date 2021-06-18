dbbf0c7f348639dec4aaa9e2fd8f1d75c91c8323
suzonbachet <suzon.bachet@gmail.com>
2021-06-18 15:22:58 +0200

    add pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'Chips';
-document.write(favoriteFood);
+let pokemonList = [
+	{name = 'Bulbasur', height = 0,7, type = ['grass', 'poison']},
+	{name = 'Charmander', height = 0.6, type = 'fire'}, 
+	{name = 'Phanpy', height = 1.08, type = 'ground'}, 
+	{name = 'Eve', height = 1, type = 'normal'} 
+	];
