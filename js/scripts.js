809dcaf3e5a6ab275fc5f386399fc80f307b12a2
suzonbachet <suzon.bachet@gmail.com>
2021-06-22 10:43:28 +0200

    add arrays and change minor error

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
-	{name = 'Bulbasur', height = 0,7, type = ['grass', 'poison']},
-	{name = 'Charmander', height = 0.6, type = 'fire'}, 
-	{name = 'Phanpy', height = 1.08, type = 'ground'}, 
-	{name = 'Eve', height = 1, type = 'normal'} 
+	{name = 'Bulbasur', height = 0.7, types = ['grass', 'poison']},
+	{name = 'Charmander', height = 0.6, types = ['fire']}, 
+	{name = 'Phanpy', height = 1.08, types = ['ground']}, 
+	{name = 'Eve', height = 1, types = ['normal']} 
 	];
