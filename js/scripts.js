731b014dd496df2c0b742911c3d5b2cab89cb052
suzonbachet <suzon.bachet@gmail.com>
2021-06-23 12:19:39 +0200

    correct object error

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 let pokemonList = [
-	{name = 'Bulbasur', height = 0.7, types = ['grass', 'poison']},
-	{name = 'Charmander', height = 0.6, types = ['fire']}, 
-	{name = 'Phanpy', height = 1.08, types = ['ground']}, 
-	{name = 'Eve', height = 1, types = ['normal']} 
+	{name: 'Bulbasur', height: 0.7, types: ['grass', 'poison']},
+	{name: 'Charmander', height: 0.6, types: ['fire']}, 
+	{name: 'Phanpy', height: 1.08, types: ['ground']}, 
+	{name: 'Eve', height: 1, types: ['normal']} 
 	];
 
 for (let i = 0; i < pokemonList.lenght; i++) {
