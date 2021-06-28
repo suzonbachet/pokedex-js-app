723ba269f61e220cb11c79e38b9e39625132ce5a
suzonbachet <suzon.bachet@gmail.com>
2021-06-28 14:51:57 +0200

    add an IIFE for the pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
-let pokemonList = [
-	{name: 'Bulbasur', height: 0.7, types: ['grass', 'poison']},
-	{name: 'Charmander', height: 0.6, types: ['fire']}, 
-	{name: 'Phanpy', height: 1.08, types: ['ground']}, 
-	{name: 'Eve', height: 1, types: ['normal']} 
-	];
+let pokemonRepository = function () {
+	let pokemonList = {};
+	{name = 'Bulbasur', height = 0.7, types = ['grass', 'poison']};
+	{name = 'Charmander', height = 0.6, types = ['fire']};
+	{name = 'Phanpy', height = 1.08, types = ['ground']}; 
+	{name = 'Eve', height = 1, types = ['normal']}; 
+	function getAll() {
+		return pokemonList;
+	}
+	function add(pokemon)
+		pokemonList.add(pokemon);
+	}
+	return {
+		getAll: getAll,
+		add: add
+	};
+	();
+
 
 
 // Create a list of Pokemon and their caracteristics
