9bcbd7291cea1231c193b07c5adc474bbe670382
suzonbachet <suzon.bachet@gmail.com>
2021-06-29 12:14:44 +0200

    change pokemonList object into array

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
 let pokemonRepository = function () {
-	let pokemonList = {};
-	{name = 'Bulbasur', height = 0.7, types = ['grass', 'poison']};
-	{name = 'Charmander', height = 0.6, types = ['fire']};
-	{name = 'Phanpy', height = 1.08, types = ['ground']}; 
-	{name = 'Eve', height = 1, types = ['normal']}; 
+	let pokemonList = [
+		{name = 'Bulbasur', height = 0.7, types = ['grass', 'poison']},
+		{name = 'Charmander', height = 0.6, types = ['fire']},
+		{name = 'Phanpy', height = 1.08, types = ['ground']}, 
+		{name = 'Eve', height = 1, types = ['normal']},
+	];
 	function getAll() {
 		return pokemonList;
 	}
-	function add(pokemon)
+	function add(pokemon) {
 		pokemonList.add(pokemon);
 	}
 	return {
 		getAll: getAll,
 		add: add
-	};
-	();
+	}();
+();
 
 
 
