3c813d02cf7e063e2c4745a0add9ef278c483dcd
suzonbachet <suzon.bachet@gmail.com>
2021-06-29 12:29:11 +0200

    fix the pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 let pokemonRepository = function () {
 	let pokemonList = [
-		{name = 'Bulbasur', height = 0.7, types = ['grass', 'poison']},
-		{name = 'Charmander', height = 0.6, types = ['fire']},
-		{name = 'Phanpy', height = 1.08, types = ['ground']}, 
-		{name = 'Eve', height = 1, types = ['normal']},
+		{name: 'Bulbasur', height: 0.7, types: ['grass', 'poison']},
+		{name: 'Charmander', height: 0.6, types: ['fire']},
+		{name: 'Phanpy', height: 1.08, types: ['ground']}, 
+		{name: 'Eve', height: 1, types: ['normal']},
 	];
 	function getAll() {
 		return pokemonList;
@@ -15,8 +15,7 @@ let pokemonRepository = function () {
 		getAll: getAll,
 		add: add
 	}();
-();
-
+};
 
 
 // Create a list of Pokemon and their caracteristics
@@ -31,3 +30,14 @@ let pokemonRepository = function () {
 }
 	});
 
+
+	getAll.forEach(function(pokemon) {
+  	console.log(name);
+// Add "Wow, that's big !" after the height of the biggest Pokemon 
+  	if (pokemon.height > 1) {
+	document.write(pokemon.name + " - height: " + pokemon.height + " - Wow, that's big!" + "<br>");
+}
+	else {
+	document.write(pokemon.name + " - height: " + pokemon.height + "<br>");
+}
+	});
