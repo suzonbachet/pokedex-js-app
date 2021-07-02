635bc95c52a0904a46b981a2bc24a80aae9d4817
suzonbachet <suzon.bachet@gmail.com>
2021-07-02 19:49:24 +0200

    add loadList and loadDetails functions

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ let pokemonRepository = (function () {
 		{name: 'Phanpy', height: 1.08, types: ['ground']}, 
 		{name: 'Eve', height: 1, types: ['normal']},
 	];
+	let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
 	function getAll() {
 		return pokemonList;
 	}
@@ -22,25 +24,61 @@ let pokemonRepository = (function () {
 		unorderedList.appendChild(listItem);
 		// Add EventListener to the button: pokemon details should be displayd after a button click
 		function showDetails(pokemon) {
-			console.log(pokemon);
+			loadDetails(pokemon).then(function () {
+				console.log(pokemon);
+			});
 		}
 		button.addEventListener('click', () => showDetails(pokemon))
 		}
+
+	function loadList() {
+		return fetch(apiUrl).then(function (response) {
+			return response.json();
+		}).then(function (json) {
+			json.results.forEach(function (item) {
+				let pokemon = {
+					name.item.name,
+					detailsUrl: item.detailsUrl
+				};
+				add(pokemon);
+			});
+		}).catch(function (e) {
+			console.error(e);
+		})
+	}
+
+	function loadDetails(item) {
+		let url = item.detailsUrl;
+		return fetch(url).then(function (response)
+	{ 
+		return reponse.json();
+	}).then(function (details) {
+		// add the details to the item
+		item.imageUrl = details.sprites.front_default;
+		item.height = details.height;
+		item.types = details.types ;
+	}).catch(function (e) {
+		console.error(e);
+	});
+	}
 
 	return {
 		getAll: getAll,
 		add: add,
 		addListItem: addListItem
+		loadList: loadList
+		loardDetails: loadDetails
 	}
+	
+	};
 })();
 
-// Add a new pokemon to the pokemonList array
-pokemonRepository.add({name: 'Tangela', height: 3.03, types: ['grass']})
-
-
+pokemonRepository.loadList().then(function() {
+	// data is loaded
 // Create a forEach loop to display every pokemon
 pokemonRepository.getAll().forEach(function(pokemon) {
 // Add addListItem function into the forEach loop
 pokemonRepository.addListItem(pokemon);
+});
 });
 
