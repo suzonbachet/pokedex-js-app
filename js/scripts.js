79bc6ffdec467ac5fe2675ba70117eceb1fe4de0
suzonbachet <suzon.bachet@gmail.com>
2021-07-03 18:07:01 +0200

    add real time form validation

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,16 @@ let pokemonRepository = (function () {
 	let pokemonList = [];
 	let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+
 	function getAll() {
 		return pokemonList;
 	}
+
+
 	function add(pokemon) {
 		pokemonList.push(pokemon);
 	}
+
 
 	function addListItem(pokemon) {
 		let unorderedList = document.querySelector(".pokemon-list");
@@ -22,6 +26,7 @@ let pokemonRepository = (function () {
 			showDetails(pokemon);
 		});
 	}
+
 
 	function loadList() {
 		return fetch(apiUrl).then(function (response) {
@@ -40,6 +45,7 @@ let pokemonRepository = (function () {
 		})
 	}
 
+
 	function loadDetails(item) {
 		let url = item.detailsUrl;
 		return fetch(url).then(function (response) { 
@@ -54,11 +60,13 @@ let pokemonRepository = (function () {
 		});
 	}
 
+
 	function showDetails(pokemon) {
 			loadDetails(pokemon).then(function () {
 				console.log(pokemon);
 			});
 	}
+
 
 	return {
 		getAll: getAll,
@@ -66,8 +74,83 @@ let pokemonRepository = (function () {
 		addListItem: addListItem,
 		loadList: loadList,
 		loardDetails: loadDetails
-	};
+	}
+
+
+	function showErrorMessage(input, message) {
+		let container = input.parentElement;
+		// The .input-wrapper
+
+		// Remove an existing error
+		let error = container.querySelector('.error-message');
+
+		if (error) {
+			container.removeChild(error);
+		}
+
+		// Add the error if the message isn't empty
+		if (message) {
+			let error = document.createElement('div');
+			error.classList.add('error-message');
+			error.innerText = message;
+			container.appendChild(error);
+		}
+	}
+
+
+	function validateEmail() {
+		let value = emailInput.value;
+		
+		if (!value) {
+			showErrorMessage(emailInput, 'Email is a required field.');
+			return false;
+		}
+
+		if (value.indexOf('@') === -1) {
+			showErrorMessage(emailInput, 'You must enter a valid email address.');
+			return false;
+		}
+
+		showErrorMessage(emailInput, null);
+		return true;
+	}
+
+
+	function validatePassword() {
+		let value = passwordInput.value;
+		
+		if (!value) {
+			showErrorMessage(passwordInput, 'Password is a required field.');
+			return false;
+		}
+
+		if (value.length < 8) {
+			showErrorMessage(passwordInput, 'The password needs to be at least 8 characters long.');
+			return false;
+		}
+
+		showErrorMessage(passwordInput, null);
+		return true;
+	}
+
+	function valideForm() {
+		let isValidEmail = validateEmail();
+		let isValidPassword = validatePassword();
+		return isValidEmail && isValidPassword;
+	}
+
+	form.addEventListener('submit', (e) => {
+		e.preventDefault(); // Do not submit to the server 
+		if (validationForm()) {
+			alert('Success!');
+		}
+	});
+
+	emailInput.addEventListener('input', validateEmail);
+	passwordInput.addEventListener('input', validatePassword);
+
 })();
+
 
 pokemonRepository.loadList().then(function() {
 // data is loaded
